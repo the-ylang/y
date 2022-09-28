9e2911c9618932593ef8e933261a3f0cf7b85321
Lil_Tim <99663083+LilTim0@users.noreply.github.com>
2022-09-28 17:10:37 +0200

    Update lexer.cpp - 28.09.2022
    Update 1 file : 
    - lexer.cpp
      - Lexer functions ready
      - To do : Add all keywords

--- src/compiler/lexer.cpp
+++ src/compiler/lexer.cpp
@@ -10,27 +10,43 @@ namespace Lexer {
 
 	class InvalidNumberLitteral : public std::exception
 	{
+		std::string m_msg;
+	public:
+
+		InvalidNumberLitteral(const std::string num_buffer)
+			: m_msg("The number " + num_buffer + " has an invalid litteral")
+		{}
+
+		virtual const char* what() const throw()
+		{
+			return m_msg.c_str();
+		}
+	};
+
+	/*class InvalidNumberLitteral : public std::exception
+	{
 		const char* what() const throw ()
 		{
 			return "The number has an invalid litteral";
 		}
-	};
+	};*/
 
 	class Token {
 	public:
 		Token(std::string code_to_tokenize) {
 			code = code_to_tokenize + END;
 			std::cout << code << std::endl;
-			doubleQuotesOpen = false;
 		}
 
 		void Run() {
-			// Iterate through line
-			while (index < code.size()) {
+			for(index = 0; index < code.size(); index) {
 				// Iterate until ' ' or end of string
+				std::string act_token_buffer = EMPTY_STRING; // Buffer for the current token
 				bool skip = false;
+				bool is_number = false;
+				bool is_string = false;
 				while (skip != true) {
-					skip = GetToken();
+					skip = GetToken(&act_token_buffer, &is_number, &is_string);
 					index += 1;
 				}
 				// Add buffers to tokens
@@ -39,8 +55,11 @@ namespace Lexer {
 		}
 
 		void PrintTokens() {
-			for (std::string iter : tokens)
-				std::cout << iter << std::endl;
+			int cpt = 0;
+			for (std::pair<std::string, int> tok : tokens) {
+				std::cout << cpt << " " << tok.first << " " << tok.second << std::endl;
+				cpt++;
+			}
 		}
 
 	private:
@@ -49,17 +68,23 @@ namespace Lexer {
 
 		// Going through
 		char value;
-		char next_value;
 		int index;
+		int map_index = 0;
 
 		// Buffers
 		std::string word_buffer;
 		std::string num_buffer;
 		std::string string_buffer;
-		bool doubleQuotesOpen;
 
 		// Output
-		std::vector<std::string> tokens;
+		std::vector<std::pair<std::string, int>> tokens;
+		/*
+		 Int meaning:
+		  > 0: Number
+		  > 1: Word
+		  > 2: String
+		  > ...
+		*/
 
 		// Constants
 		const char END = '\n';
@@ -68,36 +93,32 @@ namespace Lexer {
 		const char POINT = '.';
 		const std::string EMPTY_STRING = "";
 
-		char GetToken() {
+		char GetToken(std::string *actual_token_buffer, bool *is_number, bool *is_string) {
 			value = code[index];
-			next_value = code[index + 1];
-			if (value == DOUBLE_QUOTE)
-				doubleQuotesOpen = !doubleQuotesOpen;
-			else if (isdigit(value)) {
-				if (doubleQuotesOpen)
-					string_buffer += value;
-				else
-					num_buffer += value;
+			*actual_token_buffer += value;
+			if (value == DOUBLE_QUOTE) {
+				if(*is_number)
+					throw InvalidNumberLitteral(code.substr(index));
+				*is_string = !*is_string;
 			}
-			else if (value == POINT) {
-				if (doubleQuotesOpen)
-					string_buffer += value;
-				else {
-					std::cout << num_buffer.size() << std::endl;
-					std::cout << num_buffer << std::endl;
-					if (num_buffer.size() > 0 and num_buffer.find(value) == std::string::npos)
-						num_buffer += value;
-					else
-						throw InvalidNumberLitteral();
-				}
-			}
+			else if (*is_string)
+				string_buffer += value;
 			else if (value == SPACE or value == END)
 				return true;
-			else {
-				if(!doubleQuotesOpen)
-					word_buffer += value;
+			else if (isdigit(value)) {
+				num_buffer += value;
+				*is_number = true;
+			}
+			else if (value == POINT) {
+				if (num_buffer.size() > 0 and num_buffer.find(value) == std::string::npos)
+					num_buffer += value;
 				else
-					string_buffer += value;
+					throw InvalidNumberLitteral(*actual_token_buffer);
+			}
+			else {
+				if(*is_number)
+					throw InvalidNumberLitteral(*actual_token_buffer);
+				word_buffer += value;
 			}
 
 			return false;
@@ -105,11 +126,13 @@ namespace Lexer {
 
 		void AddBufferToTokens() {
 			if (num_buffer != EMPTY_STRING)
-				tokens.push_back(num_buffer);
+				tokens.push_back(std::make_pair(num_buffer, 0));
 			else if (word_buffer != EMPTY_STRING)
-				tokens.push_back(word_buffer);
+				tokens.push_back(std::make_pair(word_buffer, 1));
 			else if (string_buffer != EMPTY_STRING)
-				tokens.push_back(string_buffer);
+				tokens.push_back(std::make_pair(string_buffer, 2));
+
+			num_buffer = EMPTY_STRING; word_buffer = EMPTY_STRING; string_buffer = EMPTY_STRING;
 		}
 	};
 
@@ -126,7 +149,7 @@ namespace Lexer {
 
 int main()
 {
-    Lexer::Token lex  = Lexer::Token("Hello 1.23 ");
+    Lexer::Token lex  = Lexer::Token("Hello 1.23 comment tu vas ? \"ahahah\"");
     lex.Run();
 	lex.PrintTokens();
 }
