ec7ac2944e5adbb305f0d383eee7bacba17944d8
Lil_Tim <99663083+LilTim0@users.noreply.github.com>
2022-09-30 16:34:59 +0200

    Add files via upload - 2022.09.30
    Added 1 file :
    - lexer.h
      Header file for lexer.cpp
    
    Updated 1 file :
    - lexer.cpp
      Almost finished lexer
      Migration to library file

--- src/compiler/lexer.cpp
+++ src/compiler/lexer.cpp
@@ -2,7 +2,10 @@
  > Code by Icy
  > Y Lang Project
  > Github : https://github.com/the-ylang/y
+ > Lexer
 */
+
+#include "lexer.h"
 
 #include <iostream>
 #include <string>
@@ -11,162 +14,122 @@
 
 namespace Lexer {
 
-	class InvalidNumberLitteral : public std::exception
-	{
-		std::string m_msg;
-	public:
+    InvalidNumberLitteral::InvalidNumberLitteral(const std::string num_buffer)
+        : m_msg("The number " + num_buffer + " has an invalid litteral")
+    {}
 
-		InvalidNumberLitteral(const std::string num_buffer)
-			: m_msg("The number " + num_buffer + " has an invalid litteral")
-		{}
+    const char* InvalidNumberLitteral::what() const throw() {
+        return m_msg.c_str();
+    }
 
-		virtual const char* what() const throw()
-		{
-			return m_msg.c_str();
-		}
-	};
+    Lexer::Lexer(std::string code_to_tokenize) {
+        // Add the end char at the end of code if it is not already there
+        if (code_to_tokenize.back() != END)
+            code = code_to_tokenize + END;
+        else
+            code = code_to_tokenize;
 
-	class Token {
-	public:
-		Token(std::string code_to_tokenize) {
-			code = code_to_tokenize + END;
-			std::cout << code << std::endl;
-		}
+        // Initialise all the keywords
+        keywords = {
+            "int", "double", "byte", "char", "bool", "str", "const", "auto", "udf", // Types
+            "+", "-", "*", "/", "%", "**", "<<", "++", "--", // Operators
+            "and", "or", "not", "xor", // Booleans operators
+            "&", "|", "~", "^", // Bitwise operators
+            "loop", "for", "while", "do", "break", "continue", // Loops
+            "if", "elsif", "else", "switch", "case", "default", "==", "!=", // Statements
+            "class", "typedef", "public", "protected", "private", "self", "super", // Object Oriented
+            "try", "catch", "even", "raise", // Errors
+            "fn", "return", "yield", // Functions
+            "codespace", "use" // Spaces and Imports
+            // https://www.programiz.com/cpp-programming/keywords-identifiers
+        };
+    }
 
-		void Run() {
-			for(index = 0; index < code.size(); index) {
-				// Iterate until ' ' or end of string
-				std::string act_token_buffer = EMPTY_STRING; // Buffer for the current token
-				bool skip = false;
-				bool is_number = false;
-				bool is_string = false;
-				while (skip != true) {
-					skip = GetToken(&act_token_buffer, &is_number, &is_string);
-					index += 1;
-				}
-				// Add buffers to tokens
-				AddBufferToTokens();
-			}
-		}
+    // Runs the Lexer and add buffers to tokens list
+    void Lexer::Run() {
+        for (index = 0; index < code.size(); index) {
+            // Iterate until ' ' or end of string
+            std::string act_token_buffer = EMPTY_STRING; // Buffer for the current token
+            bool skip = false;
+            bool is_number = false;
+            bool is_string = false;
+            while (skip != true) {
+                skip = GetToken(&act_token_buffer, &is_number, &is_string);
+                index += 1;
+            }
+            // Add buffers to tokens
+            AddBufferToTokens();
+        }
+    }
 
-		void PrintTokens() {
-			int cpt = 0;
-			for (std::pair<std::string, int> tok : tokens) {
-				std::cout << cpt << " " << tok.first << " " << tok.second << std::endl;
-				cpt++;
-			}
-		}
+    // Print all tokens (__debug)
+    void Lexer::PrintTokens() {
+        int cpt = 0;
+        for (std::pair<std::string, int> tok : tokens) {
+            std::cout << cpt << " " << tok.first << " " << tok.second << std::endl;
+            cpt++;
+        }
+    }
 
-	private:
-		// Input
-		std::string code;
+    char Lexer::GetToken(std::string* actual_token_buffer, bool* is_number, bool* is_string) {
+        value = code[index];
+        *actual_token_buffer += value;
+        if (value == DOUBLE_QUOTE) {
+            if (*is_number)
+                throw InvalidNumberLitteral(code.substr(index));
+            *is_string = !*is_string;
+        }
+        else if (*is_string)
+            string_buffer += value;
+        else if (value == SPACE || value == END)
+            return true;
+        else if (isdigit(value)) {
+            num_buffer += value;
+            *is_number = true;
+        }
+        else if (value == POINT) {
+            if (num_buffer.size() > 0 && num_buffer.find(value) == std::string::npos)
+                num_buffer += value;
+            else
+                throw InvalidNumberLitteral(*actual_token_buffer);
+        }
+        else {
+            if (*is_number)
+                throw InvalidNumberLitteral(*actual_token_buffer);
+            word_buffer += value;
+        }
 
-		// Going through
-		char value;
-		int index;
-		int map_index = 0;
+        return false;
+    }
 
-		// Buffers
-		std::string word_buffer;
-		std::string num_buffer;
-		std::string string_buffer;
+    void Lexer::AddBufferToTokens() {
+        if (num_buffer != EMPTY_STRING)
+            tokens.push_back(std::make_pair(num_buffer, 0));
+        else if (word_buffer != EMPTY_STRING) {
+            if (std::find(keywords.begin(), keywords.end(), word_buffer) != keywords.end())
+                tokens.push_back(std::make_pair(word_buffer, 3));
+            else
+                tokens.push_back(std::make_pair(word_buffer, 1));
+        }
+        else if (string_buffer != EMPTY_STRING)
+            tokens.push_back(std::make_pair(string_buffer, 2));
 
-		// Output
-		std::vector<std::pair<std::string, int>> tokens;
-		/*
-		 Int meaning:
-		  > 0: Number
-		  > 1: Word
-		  > 2: String
-		  > ...
-		*/
+        num_buffer = EMPTY_STRING; word_buffer = EMPTY_STRING; string_buffer = EMPTY_STRING;
+    }
 
-		// Constants
-		const char END = '\n';
-		const char SPACE = ' ';
-		const char DOUBLE_QUOTE = '"';
-		const char POINT = '.';
-		const std::string EMPTY_STRING = "";
+    std::vector<std::string> Lexer::split(std::string string_to_split, char delimiter) {
+        std::vector<std::string> splitted_string;
+        int index_split = 0;
+        std::string buffer;
+        for (char iter : string_to_split) {
+            if (iter == delimiter) {
+                splitted_string.push_back(buffer);
+                index_split++;
+            }
+            else
+                buffer += iter;
+        }
 
-		char GetToken(std::string *actual_token_buffer, bool *is_number, bool *is_string) {
-			value = code[index];
-			*actual_token_buffer += value;
-			if (value == DOUBLE_QUOTE) {
-				if(*is_number)
-					throw InvalidNumberLitteral(code.substr(index));
-				*is_string = !*is_string;
-			}
-			else if (*is_string)
-				string_buffer += value;
-			else if (value == SPACE || value == END)
-				return true;
-			else if (isdigit(value)) {
-				num_buffer += value;
-				*is_number = true;
-			}
-			else if (value == POINT) {
-				if (num_buffer.size() > 0 && num_buffer.find(value) == std::string::npos)
-					num_buffer += value;
-				else
-					throw InvalidNumberLitteral(*actual_token_buffer);
-			}
-			else {
-				if(*is_number)
-					throw InvalidNumberLitteral(*actual_token_buffer);
-				word_buffer += value;
-			}
-
-			return false;
-		}
-
-		void AddBufferToTokens() {
-			if (num_buffer != EMPTY_STRING)
-				tokens.push_back(std::make_pair(num_buffer, 0));
-			else if (word_buffer != EMPTY_STRING)
-				tokens.push_back(std::make_pair(word_buffer, 1));
-			else if (string_buffer != EMPTY_STRING)
-				tokens.push_back(std::make_pair(string_buffer, 2));
-
-			num_buffer = EMPTY_STRING; word_buffer = EMPTY_STRING; string_buffer = EMPTY_STRING;
-		}
-	};
-
-	std::vector<std::string> keywords = {
-		"int", "double", "byte", "char", "bool", "str", "const", "auto", "udf", // Types
-		"+", "-", "*", "/", "%", "**", "=", "++", "--", // Operators
-		"and", "or", "not", "xor", // Booleans operators
-		"&", "|", "~", "^", // Bitwise operators
-		"loop", "for", "while", "do", "break", "continue", // Loops
-		"if", "elsif", "else", "switch", "case", "default", "==", "!=", // Statements
-		"class", "typedef", "public", "protected", "private", "self", "super", // Object Oriented
-		"try", "catch", "even", "raise", // Errors
-		"fn", "return", "yield", // Functions
-		"codespace", "use" // Spaces and Imports
-		// https://www.programiz.com/cpp-programming/keywords-identifiers
-	};
-
-	std::pair<std::string, std::string> delimiters[] = {
-		std::make_pair("{", "}"),
-		std::make_pair("[", "]"),
-		std::make_pair("(", ")"),
-		std::make_pair("/*", "*/")
-	};
+        return splitted_string;
+    }
 }
-
-int main()
-{
-	Lexer::Token lex  = Lexer::Token("Hello 1.23 comment tu vas ? \"ahahah\"");
-	lex.Run();
-	lex.PrintTokens();
-}
-
-// No link : tact churchill
-
-/*
-"(", ")", "{", "}", "[", "]", ",", ";", // Separators
-"->", "=>", // Arrow
-"++", "--", // Incrementation
-"==", "!=", "<=", ">=", // Comparators
-"!", "?", ":", // Other
-"//", "/*", "*/" // Comments
-*/
---
+++ src/compiler/lexer.h
@@ -0,0 +1,83 @@
+#ifndef __LEXER_H__
+#define __LEXER_H__
+
+/*
+ > Code by Icy
+ > Y Lang Project
+ > Github : https://github.com/the-ylang/y
+ > Lexer
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+namespace Lexer {
+
+	// Exception called when a number has an invalid litteral
+	class InvalidNumberLitteral : public std::exception
+	{
+		std::string m_msg;
+	public:
+		InvalidNumberLitteral(const std::string num_buffer);
+		virtual const char* what() const throw();
+	};
+
+	// Lexer class who iterates through the code and returns a list of tokens
+	class Lexer {
+
+	public:
+		Lexer(std::string code_to_tokenize);
+
+		// Runs the Lexer and add buffers to tokens list
+		void Run();
+
+		// Print all tokens (__debug)
+		void PrintTokens();
+
+	private:
+		// The input string (code to tokenize)
+		std::string code; // std::string Lexer::code;
+
+		// Going through
+
+		// The actual 
+		char value;
+		int index;
+
+		// Buffers
+		std::string word_buffer;
+		std::string num_buffer;
+		std::string string_buffer;
+
+		// Keywords
+		std::vector<std::string> keywords;
+
+		// Output
+		std::vector<std::pair<std::string, int>> tokens;
+
+		/*
+		 Int meaning:
+		  > 0: Number
+		  > 1: Word
+		  > 2: String
+		  > ...
+		*/
+
+		// Constants
+		const char END = '\n';
+		const char SPACE = ' ';
+		const char DOUBLE_QUOTE = '"';
+		const char POINT = '.';
+		const std::string EMPTY_STRING = "";
+
+		char GetToken(std::string* actual_token_buffer, bool* is_number, bool* is_string);
+
+		void AddBufferToTokens();
+
+		std::vector<std::string> split(std::string string_to_split, char delimiter);
+	};
+}
+
+#endif // __LEXER_H__
