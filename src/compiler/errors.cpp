98369ea661d55eccdce8740c25e3bfcc3c575597
Timeo Perrinjaquet <99663083+LilTim0@users.noreply.github.com>
2022-11-21 14:20:59 +0100

    🔄 Updated error gestion system
    2022.11.21

--- src/compiler/errors.cpp
+++ src/compiler/errors.cpp
@@ -1,10 +1,71 @@
 #include <iostream>
 
-#include "errors.cpp"
+#include "errors.hpp"
 
-Y::outputs::errors::Error::Error();
-Y::outputs::errors::Error::~Error();
-Y::outputs::warnings::Warning::Warning();
-Y::outputs::warnings::Warning::~Warning();
-Y::outputs::informations::Information::Information();
-Y::outputs::informations::Information::~Information();
+// General
+
+void Y::outputs::Output::Show(Y::outputs::OUT outpt, std::string type) {
+  std::cout << type << " : [" << outpt.line << "] " << outpt.message << " at : "
+            << outpt.line[outpt.start_to_end[0], outpt.start_to_end[1]]
+            << std::endl;
+}
+
+// Errors
+
+Y::outputs::errors::Error::Error(int level) { error_level = level; }
+
+Y::outputs::errors::SyntaxError::SyntaxError(std::string line, int line_number, int start_to_end[2], int error_level) : Error(error_level) {
+  	Show(
+      	{
+			"Invalid syntax",
+			line,
+			line_number,
+			*start_to_end
+	  	},
+	  	"SyntaxError"
+	);
+}
+
+Y::outputs::errors::InvalidLitteralError::InvalidLitteralError(std::string line, int line_number, int start_to_end[2], int error_level) : Error(error_level) {
+  	Show(
+      	{
+	        "Invalid litteral",
+		  	line,
+	        line_number,
+	        *start_to_end
+	  	},
+		"InvalidLitteralError"
+  	);
+}
+
+// Warnings
+
+Y::outputs::warnings::Warning::Warning(int level) { warn_level = level; }
+
+Y::outputs::warnings::UnusedValueWarning::UnusedValueWarning(std::string line, int line_number, int start_to_end[2], int warn_level) : Warning(warn_level) {
+	Show(
+	  	{
+			"Value declared but never used",
+			line,
+			line_number,
+			*start_to_end
+	  	},
+	  	"UnusedValueWarning"
+  	);
+}
+
+// Informations
+
+Y::outputs::informations::Information::Information() {}
+
+Y::outputs::informations::UnusedFileImport::UnusedFileImport(std::string line, int line_number, int start_to_end[2]) {
+	Show(
+		{
+			"File imported but never used",
+			line,
+			line_number,
+			*start_to_end
+		},
+		"UnusedFileImport"
+	);
+}
--- src/compiler/errors.hpp
+++ src/compiler/errors.hpp
@@ -1,27 +1,75 @@
+#include <iostream>
+#include <string>
+
 #ifndef __ERRORS_HPP__
 	#define __ERRORS_HPP__
 
 	namespace Y {
 		namespace outputs {
+			struct OUT {
+				std::string message;
+				std::string line;
+				int line_number;
+				int start_to_end[2];
+			};
+			class Output {
+			public:
+				Output();
+				void Show(OUT outpt, std::string type);
+			};
 			namespace errors {
-				class Error {
+				class Error : public Output {
+				private:
+					int error_level;
 				public:
-					Error();
-					~Error();
+					Error(int level);
+				};
+				class SyntaxError : public Error {
+				public:
+					SyntaxError(
+						std::string line,
+						int line_number,
+						int start_to_end[2],
+						int error_level
+					);
+				};
+				class InvalidLitteralError : public Error {
+				public:
+					InvalidLitteralError(
+						std::string line,
+						int line_number,
+						int start_to_end[2],
+						int error_level
+					);
 				};
 			}
 			namespace warnings {
-				class Warning {
+				class Warning : public Output {
+				private:
+					int warn_level;
 				public:
-					Warning();
-					~Warning();
+					Warning(int level);
+				};
+				class UnusedValueWarning : public Warning {
+					UnusedValueWarning(
+						std::string line,
+						int line_number,
+						int start_to_end[2],
+						int warn_level
+					);
 				};
 			}
 			namespace informations {
-				class Information {
+				class Information : public Output {
 				public:
 					Information();
-					~Information();
+				};
+				class UnusedFileImport : public Information {
+					UnusedFileImport(
+						std::string line,
+						int line_number,
+						int start_to_end[2]
+					);
 				};
 			}
 		}
