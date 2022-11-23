57f2eb89de12c6baa5c8ac16decde81e7e6cc756
Timeo Perrinjaquet <99663083+LilTim0@users.noreply.github.com>
2022-11-23 14:33:57 +0100

    🔧 Fixed error gestion system
    2022.11.23

--- src/compiler/errors.cpp
+++ src/compiler/errors.cpp
@@ -4,68 +4,52 @@
 
 // General
 
+Y::outputs::Output::Output() { return; }
+
 void Y::outputs::Output::Show(Y::outputs::OUT outpt, std::string type) {
-  std::cout << type << " : [" << outpt.line << "] " << outpt.message << " at : "
-            << outpt.line[outpt.start_to_end[0], outpt.start_to_end[1]]
+  std::cout << type << " : [" << outpt.line_number << "] " << outpt.message
+            << " at : "
+            << outpt.line.substr(outpt.start_to_end[0], outpt.start_to_end[1] - outpt.start_to_end[0])
             << std::endl;
 }
 
 // Errors
 
-Y::outputs::errors::Error::Error(int level) { error_level = level; }
+Y::outputs::errors::Error::Error(int level) : Output() { error_level = level; }
 
-Y::outputs::errors::SyntaxError::SyntaxError(std::string line, int line_number, int start_to_end[2], int error_level) : Error(error_level) {
-  	Show(
-      	{
-			"Invalid syntax",
-			line,
-			line_number,
-			*start_to_end
-	  	},
-	  	"SyntaxError"
-	);
+Y::outputs::errors::SyntaxError::SyntaxError(std::string line, int line_number,
+                                             int start_to_end[2],
+                                             int error_level)
+    : Error(error_level) {
+  Show({"Invalid syntax", line, line_number, *start_to_end}, "SyntaxError");
 }
 
-Y::outputs::errors::InvalidLitteralError::InvalidLitteralError(std::string line, int line_number, int start_to_end[2], int error_level) : Error(error_level) {
-  	Show(
-      	{
-	        "Invalid litteral",
-		  	line,
-	        line_number,
-	        *start_to_end
-	  	},
-		"InvalidLitteralError"
-  	);
+Y::outputs::errors::InvalidLitteralError::InvalidLitteralError(
+    std::string line, int line_number, int start_to_end[2], int error_level)
+    : Error(error_level) {
+  Show({"Invalid litteral", line, line_number, *start_to_end},
+       "InvalidLitteralError");
 }
 
 // Warnings
 
-Y::outputs::warnings::Warning::Warning(int level) { warn_level = level; }
+Y::outputs::warnings::Warning::Warning(int level) : Output() {
+  warn_level = level;
+}
 
-Y::outputs::warnings::UnusedValueWarning::UnusedValueWarning(std::string line, int line_number, int start_to_end[2], int warn_level) : Warning(warn_level) {
-	Show(
-	  	{
-			"Value declared but never used",
-			line,
-			line_number,
-			*start_to_end
-	  	},
-	  	"UnusedValueWarning"
-  	);
+Y::outputs::warnings::UnusedValueWarning::UnusedValueWarning(
+    std::string line, int line_number, int start_to_end[2], int warn_level)
+    : Warning(warn_level) {
+  Show({"Value declared but never used", line, line_number, *start_to_end},
+       "UnusedValueWarning");
 }
 
 // Informations
 
-Y::outputs::informations::Information::Information() {}
+Y::outputs::informations::Information::Information() : Output() {}
 
-Y::outputs::informations::UnusedFileImport::UnusedFileImport(std::string line, int line_number, int start_to_end[2]) {
-	Show(
-		{
-			"File imported but never used",
-			line,
-			line_number,
-			*start_to_end
-		},
-		"UnusedFileImport"
-	);
+Y::outputs::informations::UnusedFileImport::UnusedFileImport(
+    std::string line, int line_number, int start_to_end[2]) {
+  Show({"File imported but never used", line, line_number, *start_to_end},
+       "UnusedFileImport");
 }
--- src/compiler/errors.hpp
+++ src/compiler/errors.hpp
@@ -51,6 +51,7 @@
 					Warning(int level);
 				};
 				class UnusedValueWarning : public Warning {
+				public:
 					UnusedValueWarning(
 						std::string line,
 						int line_number,
@@ -65,6 +66,7 @@
 					Information();
 				};
 				class UnusedFileImport : public Information {
+				public:
 					UnusedFileImport(
 						std::string line,
 						int line_number,
